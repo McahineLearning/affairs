03258641a9f47bfffc9b2722dca52d8f67a754db
Ankan Datta <ankan.d@nabler.com>
2021-10-21 23:50:42 +0530

    webapp

--- template.py
+++ template.py
@@ -8,7 +8,14 @@ dirs = [
     'saved_models',
     'src',
     'report',
-    'tests'
+    'tests',
+    'prediction_service',
+    'webapp',
+    os.path.join('prediction_service','model'),
+    os.path.join('webapp','static'),
+    os.path.join('webapp','templates'),
+    os.path.join('webapp\\static','css'),
+    os.path.join('webapp\\static','script')
 ]
 
 
@@ -23,10 +30,18 @@ files = [
     os.path.join('src', '__init__.py'),
     os.path.join('src', 'make_data.py'),
     "tox.ini",
+    "app.py",
     os.path.join('tests', 'test.py'),
     os.path.join('tests', 'test_config.py'),
     os.path.join('tests', '__init__.py'),
-    os.path.join('tests', 'conftest.py')
+    os.path.join('tests', 'conftest.py'),
+    os.path.join('prediction_service','__init__.py'),
+    os.path.join('prediction_service','prediction_service.py'),
+    os.path.join('webapp\\templates', 'index.html'),
+    os.path.join('webapp\\templates', 'base.html'),
+    os.path.join('webapp\\templates', '404.html'),
+    os.path.join('webapp\\static\\script', 'index.js'),
+    os.path.join('webapp\\static\\css', 'main.css')
 
 
 ]
